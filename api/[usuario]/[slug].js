d680347de936758faa98a5263fe78800f462fd69
subidanalyzer <subidanalyzer@gmail.com>
2026-03-07 00:54:26 -0300

    testes2
    testes2

--- api/[usuario]/[slug].js
+++ api/[usuario]/[slug].js
@@ -1,10 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
 export default async function handler(req, res) {
 
-  const { usuario, slug } = req.query;
+  const { usuario } = req.query;
+
+  const { data, error } = await supabase
+    .from("usuario")
+    .select("*")
+    .eq("nm_usuario", usuario);
 
   return res.json({
     usuario,
-    slug
+    resultado: data,
+    erro: error
   });
 
 }
